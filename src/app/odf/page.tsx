1dddcaff7434f627adf69e85bb4c559e3b1c35b2
hb9397 <hb9397@naver.com>
2025-05-19 18:00:20 +0900

    set -ing

---
+++ src/app/odf/page.tsx
@@ -0,0 +1,14 @@
+// import React, {useEffect, useState} from 'react';
+
+const Page = () => {
+
+    var temp = new odf.coord
+
+    return (
+        <div id="map" className="odf-view" style={{height: '550px'}} >
+
+        </div>
+    );
+};
+
+export default Page;
