aa7acf011b81272955e5095946ca4058ba95db76
hb9397 <hb9397@naver.com>
2025-05-22 16:52:48 +0900

    temp

--- src/lib/odfGeoserverLayerList.ts
+++ src/lib/odfGeoserverLayerList.ts
@@ -6,7 +6,7 @@ type GeoserverLayerList = {
 
 export const geoserverLayerList = {
     odfPracY0001Wms : { name: '용산구 관광지', layer: 'odf-prac:Y0001', type: 'wms', style: null },
-    F : { name: '용산구 관광지', layer: 'odf-prac:Y0001', type: 'wfs', style: geoserverWfsLayerStyle.YP0001 },
+    odfPracY0001Wfs : { name: '용산구 관광지', layer: 'odf-prac:Y0001', type: 'wfs', style: geoserverWfsLayerStyle.YP0001 },
     odfPracY0002Wms : { name: '용산구 관광지(길)', layer: 'odf-prac:Y0002', type: 'wms', style: null,  },
     odfPracY0002Wfs : { name: '용산구 관광지(길)', layer: 'odf-prac:Y0002', type: 'wfs', style: geoserverWfsLayerStyle.YP0002 },
     odfPracY0003Wms : { name: '용산구 지역특화거리', layer: 'odf-prac:Y0003', type: 'wms', style: null },
